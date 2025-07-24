8dfe81e75087f8e01bd2580f174035aebf8b8b04
prince-0303 <princebinil2003@gmail.com>
2025-07-24 17:13:06 +0530

    🖌️ Added templates, static files

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Category, Customers, Product, Order
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Customers)
+admin.site.register(Product)
+admin.site.register(Order)
