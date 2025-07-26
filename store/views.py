a79de76f5070d8231831a0632bed3cc515beb7f8
prince-0303 <princebinil2003@gmail.com>
2025-07-26 15:51:01 +0530

    ✍️ Added login, logout functionality

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout  #inbuilt authenticate library
+from django.contrib import messages  # this is used to show some sort of messages during login, logout etc....
+
 # Create your views here.
 
 def home(request):
@@ -8,3 +11,23 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You have been logged in....'))
+            return redirect('home')
+        else:
+            messages.success(request, ('There was an error, please try again..'))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, ('You have logged out... Thanks for stopping by......'))
+    return redirect('home')
